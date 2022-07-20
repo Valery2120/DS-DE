dee195f86ce20c6de9cf1582b63dce42dcdda456
kachura1986 <kachura1986gmail.com>
2022-07-20 09:46:22 +0300

    Updated

--- oop/flat.py
+++ oop/flat.py
@@ -3,7 +3,7 @@ class Address:
     Class Address stores information about flat address.
     """
 
-    def __init__(self, street: str, house: str, flat:str) -> None:
+    def __init__(self, street: str, house: str, flat: str) -> None:
         """
          Init Addres object with it's parameters.
 
@@ -29,7 +29,7 @@ class Room:
     Сlass calculates square and volume of the flat.
     """
 
-    def __init__(self, width: int|float, length: int|float, height: int|float) -> None:
+    def __init__(self, width: int | float, length: int | float, height: int | float) -> None:
         """
          Init Room object with it's parameters.
 
@@ -65,6 +65,7 @@ class Room:
         print(f"ширина - {self.width}м, длина - {self.length}м, высота - {self.height}м")
 
 
+## Лучше описывать параметры класса в документации.
 class Flat:
     """
     Сlass defines square, volume and price of the flat.
@@ -78,7 +79,7 @@ class Flat:
         Init Flat object with it's parameters.
             :param street: Street name.
             :param house: House number.
-            :param flat: Flat number.
+            :param flat_number: Flat number.
         """
 
         self.address = Address(street, house, flat_number)
@@ -135,7 +136,7 @@ class Flat:
         address, cost, square and volume of the flat;
         name, width, length and height of the room.
         """
-        
+
         self.show_brief()
 
         if self.rooms:
@@ -146,7 +147,6 @@ class Flat:
 
 if __name__ == "__main__":
 
-
     flat_01 = Flat('Советская', '1', '42')
     flat_01.add_room('Комната', 3., 3.6, 2.5)
     flat_01.add_room('Комната', 2.9, 3.9, 2.5)
@@ -155,16 +155,16 @@ if __name__ == "__main__":
     flat_01.add_room('Туалет', 1., 1.8, 2.5)
     flat_01.add_room('Ванна', 2.1, 1.3, 2.5)
 
-
     flat_02 = Flat('Билецкого', '45', '35')
     flat_02.add_room('Комната', 3.7, 2.1, 2.6)
     flat_02.add_room('Кухня', 2.2, 1.7, 2.6)
     flat_02.add_room('Туалет', 1.1, 1.7, 2.6)
     flat_02.add_room('Ванна', 2.3, 1.7, 2.6)
 
+    ## Кортеж неизменяемая последовательность, поэтому если ты захочешь добавить квартиру – придется создавать новый кортеж.
+    ## Здесь лучше использовать список.
     flats = (flat_01, flat_02)
 
     for flat in flats:
         flat.show_full()
         print()
-
