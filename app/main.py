704b6f059e2a3f796e5f46817d98a7fb85951a36
Valery2120 <81355021+Valery2120@users.noreply.github.com>
2022-09-20 23:26:15 +0300

    Report
    I've made an api with two endpoints:
    1. POST request, sends a list of cities in alphabetical order that are in the database;
    2. accepts the city, sends up to 10 of the most expensive apartments in this city.
    Working on the project, I used Flask, Flask-SQLAlchemy, Flask-RECTX.
    I've changed the structure of the project.

--- app/main.py
+++ app/main.py
@@ -1,1 +1,63 @@
+from sqlalchemy import func
+import json
+from __init__ import app
+from flask import Flask, jsonify, request
+from flask_restx import Resource, Api
 
+
+
+
+api = Api(app,
+    version="1.0",
+    title="Property database",
+    description="Property analisis",
+)
+
+from models import *
+
+flat_cost = Room.query.with_entities(Room.flat_id, (func.sum(Room.height*Room.width)*600).label("cost")).group_by(Room.flat_id).all()
+
+cities = City.query.all()
+body = []
+for city in cities:
+    body.append(city.city)
+
+@api.route('/request-cities')
+class get_City(Resource):
+    def post(self):
+        """List of cities"""
+        body.sort()
+        return jsonify(body)
+
+@api.route('/request-cities/10most-expensive-flats/<city>')
+class get_flat(Resource):
+    def post(self, city):
+        """Fetch 10 the most expensive flats in the city"""
+        request_city = City.query.filter(City.city == city).first()
+        streets = Street.query.all()
+
+        city_streets = []
+        for street in streets:
+            if street.city_id == request_city.city_id:
+                city_streets.append(street)
+
+        flats = Flat.query.all()
+
+        serialized1 = []
+
+        for street in city_streets:
+            for flat in flats:
+                for cost in flat_cost:
+                    if (flat.street_id == street.street_id) and (flat.flat_id == cost.flat_id):
+                        serialized1.append({
+                            'id': flat.flat_id,
+                            'street': street.street,
+                            'house №': flat.house_number,
+                            'flat №': flat.flat_number,
+                            'cost': round(cost.cost, 2)
+                        })
+        newlist = sorted(serialized1, key=lambda d: d['cost'], reverse=True)
+        return  jsonify(newlist[0:10])
+
+if __name__ == '__main__':
+    app.run()
---
+++ app/models.py
@@ -0,0 +1,163 @@
+from __init__ import db
+
+
+class Sex(db.Model):
+    __tablename__ = 'sex'
+
+    sex_id = db.Column(db.Integer, primary_key=True)
+    sex = db.Column(db.String)
+
+    owners = db.relationship("Owner", backref='owners')
+
+    def __init__(self, sex: str) -> None:
+        self.sex = sex
+
+
+class Nationality(db.Model):
+    __tablename__ = 'nationality'
+
+    nationality_id = db.Column(db.Integer, primary_key=True)
+    nationality = db.Column(db.String)
+
+    # owners = db.relationship("Owner")
+
+    def __init__(self, nationality: str) -> None:
+        self.nationality = nationality
+
+
+association_table = db.Table('ownership', db.Model.metadata,
+    db.Column('owner_id', db.ForeignKey('owner.owner_id'), primary_key=True),
+    db.Column('flat_id', db.ForeignKey('flat.flat_id'), primary_key=True)
+)
+
+
+class Owner(db.Model):
+    __tablename__ = 'owner'
+
+    owner_id = db.Column(db.Integer, primary_key=True)
+    last_name = db.Column(db.String)
+    first_name = db.Column(db.String)
+    second_name = db.Column(db.String)
+    day_of_birth = db.Column(db.Integer)
+    month_of_birth = db.Column(db.Integer)
+    year_of_birth = db.Column(db.Integer)
+    sex_id = db.Column(db.Integer, db.ForeignKey('sex.sex_id'))
+    nationality_id = db.Column(db.Integer, db.ForeignKey('nationality.nationality_id'))
+    phone_number = db.Column(db.String)
+    email = db.Column(db.String)
+
+    flats = db.relationship(
+         "Flat",
+         secondary=association_table,
+         back_populates="owners")
+
+    def __init__(self, last_name: str, first_name: str, second_name: str, day_of_birth: int, month_of_birth: int, year_of_birth: int) -> None:
+        self.last_name = last_name
+        self.first_name = first_name
+        self.second_name = second_name
+        self.day_of_birth = day_of_birth
+        self.month_of_birth = month_of_birth
+        self.year_of_birth = year_of_birth
+
+
+class Country(db.Model):
+    __tablename__ = 'country'
+
+    country_id = db.Column(db.Integer, primary_key=True)
+    country = db.Column(db.String)
+
+    regions = db.relationship("Region")
+    passports = db.relationship("Passport")
+
+    def __init__(self, country: str) -> None:
+        self.country = country
+
+
+class Passport(db.Model):
+    __tablename__ = 'passport'
+
+    passport_id = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('owner.owner_id'))
+    date_of_issue = db.Column(db.String)
+    date_of_expiration = db.Column(db.String)
+    country_id = db.Column(db.Integer, db.ForeignKey('country.country_id'))
+
+    def __init__(self, passport_id: int, date_of_issue: str, date_of_expiration: str) -> None:
+        self.passport_id = passport_id
+        self.date_of_issue = date_of_issue
+        self.date_of_expiration = date_of_expiration
+
+
+class Region(db.Model):
+    __tablename__ = 'region'
+
+    region_id = db.Column(db.Integer, primary_key=True)
+    region = db.Column(db.String)
+    country_id = db.Column(db.Integer, db.ForeignKey('country.country_id'))
+
+    cities = db.relationship("City")
+
+    def __init__(self, region: str) -> None:
+        self.region = region
+
+
+class City(db.Model):
+    __tablename__ = 'city'
+
+    city_id = db.Column(db.Integer, primary_key=True)
+    city = db.Column(db.String)
+    region_id = db.Column(db.Integer, db.ForeignKey('region.region_id'))
+
+    streets = db.relationship("Street")
+
+    def __init__(self, city: str) -> None:
+        self.city = city
+
+
+class Street(db.Model):
+    __tablename__ = 'street'
+
+    street_id = db.Column(db.Integer, primary_key=True)
+    street = db.Column(db.String)
+    city_id = db.Column(db.Integer, db.ForeignKey('city.city_id'))
+
+    def __init__(self, street: str) -> None:
+        self.street = street
+
+
+class Room(db.Model):
+    __tablename__ = 'room'
+
+    room_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    length = db.Column(db.Float)
+    width = db.Column(db.Float)
+    height = db.Column(db.Float)
+    flat_id = db.Column(db.Integer, db.ForeignKey('flat.flat_id'))
+
+    def __init__(self, name: str, width: int | float, length: int | float, height: int | float) -> None:
+        self.name = name
+        self.width = width
+        self.length = length
+        self.height = height
+
+
+class Flat(db.Model):
+    __tablename__ = 'flat'
+
+    flat_id = db.Column(db.Integer, primary_key=True)
+    house_number = db.Column(db.Integer)
+    flat_number = db.Column(db.Integer)
+    floor = db.Column(db.Integer)
+    street_id = db.Column(db.Integer, db.ForeignKey('street.street_id'))
+
+    rooms = db.relationship("Room")
+    owners = db.relationship(
+        "Owner",
+        secondary=association_table,
+        back_populates="flats")
+
+    def __init__(self, house_number: int, flat_number: int, floor: int) -> None:
+        self.house_number = house_number
+        self.flat_number = flat_number
+        self.floor = floor
